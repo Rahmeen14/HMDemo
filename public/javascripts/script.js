4a60f7310f7b1012ff673a0c23b5c5d04009369d
sakshamsaxena <ontenth@live.com>
2016-04-09 18:24:33 +0530

    Added upvote feature

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,27 @@
+var upvotes = [];
 $(document).ready(function () {
 	$('.slider').slider({
 		full_width: true
+	});
+	$("i.tiny").click(function (e) {
+		var key = $(this).prev().attr("href");
+		var iti, upvote;
+		console.log(key);
+		for (var x = 0; x < $("i.tiny").length; x++)
+			if ($(this).is($("i.tiny")[x]))
+				iti = x;
+		console.log(iti);
+
+		if (!upvotes[iti]) {
+			upvotes[iti] = {
+				"prodId": key,
+				"upvote": 1
+			}
+		}
+
+		else
+			upvotes[iti].upvote += 1;
+		console.log($(this));
+		$(this).next().html(upvotes[iti].upvote);
 	});
 });
